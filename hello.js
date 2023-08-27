966e1d6b5cdaaffdd991a5b08e7c6c1c79f33d1c
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2023-08-27 21:30:21 +0600

    fsf

---
+++ hello.js
@@ -0,0 +1,17 @@
+var number1 = 0.1;
+var number2 = 0.2;
+//number1 = ''+number1;
+var total = number1 + number2;
+total = total.toFixed(1);
+console.log(total);
+
+//part-8
+var price1 = 25;
+var price3 = 35;
+//var total = price1 % price3;
+price3++;
+console.log(price3);
+
+var namee = "alo";
+var result = namee + ' ' + price3;
+console.log(result);
