17c748663b5ceb3725f05b50c6e8f17158518fef
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2023-08-28 14:03:39 +0600

    sdf

---
+++ javascpp-2.js
@@ -0,0 +1,27 @@
+/*var friendAge = [15,15,55,155,55];
+
+var sonaliAge = friendAge[2];
+
+friendAge[1] = 21;
+
+//var position = friendAge.indexOf(21);
+
+console.log(friendAge);
+
+friendAge.push(45);
+friendAge.push(77);
+
+console.log(friendAge);
+
+friendAge.unshift(71);
+friendAge.shift();
+friendAge.sort();
+var part = friendAge.slice(2);
+console.log(part);*/
+
+var num=0;
+
+while(num<10){
+    console.log("i am lalin");
+    num++;
+}
