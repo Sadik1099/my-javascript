28bc92e748f04fb4b0ca102554e56614cc53ff67
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2023-08-28 13:23:53 +0600

    fsdf

--- conditional.js
+++
@@ -1,9 +0,0 @@
-var biscuitsPrice = 12;
-if(biscuitsPrice < 10){
-    console.log("I will eat the biscuits");
-}
-else{
-    console.log("Khali cha is better");
-}
-var date = new Date('1971-12-16');
-console.log(date);
