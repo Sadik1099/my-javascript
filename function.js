3b5e89af6aa66333b583da10a9c584b611a48934
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2023-08-28 16:53:46 +0600

    re

---
+++ function.js
@@ -0,0 +1,22 @@
+function myFirstF(){
+    console.log("jan o baby");
+    console.log("I love u so much moni");
+
+}
+
+myFirstF();
+
+function doublrIt(num){
+    var result = num*2;
+    return result;
+}
+var first = doublrIt(5);
+var second = doublrIt(500);
+var total = first + second;
+console.log(total);
+
+function add(num1, num2){
+    return num1+num2;
+}
+var sum = add(15,66);
+console.log(sum);
---
+++ object.js
@@ -0,0 +1,7 @@
+var student = {id:121, phone:1745, name:"Lalin"};
+var student2 = {id:1221, phone:1735645, name:"Lamoni"};
+
+var phoneNO = student.phone;
+
+console.log(phoneNO);
+console.log(student2);
