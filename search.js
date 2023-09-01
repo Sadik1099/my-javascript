1b30424dd0e7120d95ef8a1de636929d4fd464bb
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2023-09-01 17:35:17 +0600

    dfsafasdf

--- search.js
+++ search.js
@@ -17,3 +17,34 @@ else
 }
 
 console.log(lyrics.startsWith('Tumi'));
+
+const chars = lyrics.split('');
+//console.log(chars);
+
+const partial = lyrics.slice(5,9);
+console.log(partial);
+const partial2 = lyrics.substring(5,9);
+console.log(partial2);
+
+
+
+function maxInArray(numbers){
+    
+    let largest = numbers[0];
+    for(let i=0; i < numbers.length; i++)
+    {
+        
+        const index = i;
+        const elements = numbers[index];
+        if(elements > largest)
+        {
+            largest = elements;
+        }
+        
+    }
+    return largest;
+}
+
+const heights = [167,554,646,323,665,656];
+const tallest = maxInArray(heights);
+console.log('Tallest person is: ', tallest);
