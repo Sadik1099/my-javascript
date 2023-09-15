e8e894a5efaf02d027d52db4ac89ed7245e70efe
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2023-09-15 12:04:37 +0600

    dfas

---
+++ porer gulo/argument.js
@@ -0,0 +1,9 @@
+// function addNumber( num1, num2)
+// {
+//     console.log(arguments[2]);
+//     return num1+num2;
+// }
+
+// var result = addNumber(4,3,6,48);
+// console.log(result);
+
--- porer gulo/revisite.js
+++ porer gulo/revisite.js
@@ -1,19 +1,33 @@
-// function evdenify(num){
+// // function evdenify(num){
    
-//    var result;
-//     if(num % 2 ==0)
-//    {
-//     result = num;
-//    }
-//    else{
-//     result = num*2;
-//    }
-//    return result;
+// //    var result;
+// //     if(num % 2 ==0)
+// //    {
+// //     result = num;
+// //    }
+// //    else{
+// //     result = num*2;
+// //    }
+// //    return result;
+// // }
+
+// // var result = evdenify(5);
+// // var square = result*result;
+// // console.log(square);
+
+// // callback Function//
+// function explin_callback(name, age, task)
+// {
+//     console.log('hello ', name);
+//     console.log('Your age ', age);
+//     var fage = washHand(task);
+//     console.log('father age is ', fage);
 // }
 
-// var result = evdenify(5);
-// var square = result*result;
-// console.log(square);
+// function washHand(task)
+// {
+//        return task*3;
+// }
 
-// callback Function//
+// explin_callback('lalin', 23,12);
 
