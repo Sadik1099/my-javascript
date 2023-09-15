d11ecc126269d0806eb69f0dfc541db5bcd08d06
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2023-09-15 10:47:45 +0600

    dfsf

---
+++ porer gulo/revisite.js
@@ -0,0 +1,19 @@
+// function evdenify(num){
+   
+//    var result;
+//     if(num % 2 ==0)
+//    {
+//     result = num;
+//    }
+//    else{
+//     result = num*2;
+//    }
+//    return result;
+// }
+
+// var result = evdenify(5);
+// var square = result*result;
+// console.log(square);
+
+// callback Function//
+
